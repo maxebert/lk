e84e1e21333dcc0f227dfddb0e50045180649e8b
maxebert <36116137+maxebert@users.noreply.github.com>
2018-02-03 23:22:55 +0100

    Add files via upload

---
+++ Global/Util.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using GrandTheftMultiplayer.Server;
+using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Constant;
+using GrandTheftMultiplayer.Server.Managers;
+using GrandTheftMultiplayer.Shared;
+using GrandTheftMultiplayer.Shared.Math;
+
+namespace IcaroRPG.Global
+{
+    public static class Util
+    {
+        public static string Repeat(string str, int times)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < times; i++)
+            {
+                sb.Append(str);
+            }
+
+            return sb.ToString();
+        }
+
+        public static void TeleportWithFade(Client player, Vector3 targetpos)
+        {
+            if (player.vehicle != null) return;
+           // API.shared.sendNativeToPlayer(player, Hash.DO_SCREEN_FADE_OUT, 200);
+          //  API.shared.delay(300, true, () => {
+                API.shared.setEntityPosition(player.handle, targetpos);
+             //   API.shared.sendNativeToPlayer(player, Hash.DO_SCREEN_FADE_IN, 1000);
+         //   });
+        }
+
+        public static bool IsInRangeOf(Vector3 playerPos, Vector3 target, float range)
+        {
+            var direct = new Vector3(target.X - playerPos.X, target.Y - playerPos.Y, target.Z - playerPos.Z);
+            var len = direct.X * direct.X + direct.Y * direct.Y + direct.Z * direct.Z;
+            return range * range > len;
+        }
+    }
+}
